1fa0ec2a2b383d99db50abf24f68304f28cadee0
Sebastian <sebcyde@hotmail.com>
2023-05-23 19:10:18 +0100

    Git revert due to bugs

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-// File Control
-const { watchJapaneseFiles } = require('./Functions/FileControl/Japanese');
-const { watchExeFiles } = require('./Functions/FileControl/Executables');
-const { watchImageFiles } = require('./Functions/FileControl/Images');
+const {
+	watchJapaneseFiles,
+	watchImageFiles,
+	watchExeFiles,
+} = require('./FileWatcher');
+
 const japaneseWatcher = watchJapaneseFiles();
 const imageWatcher = watchImageFiles();
-const ExecWatcher = watchExeFiles();
-
-// System Control
-// const { NetworkWatch } = require('./Functions/SystemControl/Network');
-const { MemoryWatch } = require('./Functions/SystemControl/Memory');
-const { DiskWatch } = require('./Functions/SystemControl/Disk');
-const { CPUWatch } = require('./Functions/SystemControl/CPU');
-// NetworkWatch();
-MemoryWatch();
-DiskWatch();
-CPUWatch();
-
-
-// Updates
+const exeWatcher = watchExeFiles();
